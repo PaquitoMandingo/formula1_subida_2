1654c868a7720a4d0db672adf1fea567aac52cd9
Figueredo <franfigueredorubio@outlook.es>
2023-01-11 09:23:33 +0100

    Actualizacion 11/01/2023 Prubas empezadas y abriendo ventanas y escribiendo un poco

--- src/main/test/Controladors/HelloControllerTest.java
+++ src/main/test/Controladors/HelloControllerTest.java
@@ -1,11 +1,13 @@
 package Controladors;
 
+import com.example.formula1.HelloApplication;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxAssert;
 import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
@@ -18,6 +20,8 @@ import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxRobot;
+import org.testfx.matcher.base.WindowMatchers;
+import org.testfx.matcher.control.LabeledMatchers;
 
 import java.io.IOException;
 
@@ -31,7 +35,9 @@ class HelloControllerTest {
 
     @Start
     public void start(Stage stage) throws IOException {
-        mainroot = (Pane) FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        System.out.println(getClass());
+
+        mainroot =  FXMLLoader.load(HelloApplication.class.getResource("hello-view.fxml"));
         mainstage = stage;
         stage.setTitle("Hello!");
         stage.setScene(new Scene(mainroot));
@@ -39,9 +45,30 @@ class HelloControllerTest {
     }
 
 
+
     @Test
-    void nuevaEscuderia() {
-            robot.clickOn("#tfRellenar");
-            robot.write("Hace algo soslo");
-        }
+    void insertarDatosTest(FxRobot robot){
+        robot.clickOn("#nuevaEscuderiaTest");
+        robot.sleep(1000);
+
+
+        robot.clickOn("#tfCodEscuderia");
+        robot.write("67670");
+        robot.clickOn("#tfCodEscuderia");
+        robot.write("67670");
+        robot.clickOn("#tfCodEscuderia");
+        robot.write("67670");
+        robot.clickOn("#tfCodEscuderia");
+        robot.write("67670");
+        robot.clickOn("#tfCodEscuderia");
+        robot.write("67670");
+        robot.clickOn("#tfCodEscuderia");
+        robot.write("67670");
+        robot.clickOn("#tfCodEscuderia");
+        robot.write("67670");
+        robot.clickOn("#tfCodEscuderia");
+        robot.write("67670");
+        robot.clickOn("#tfCodEscuderia");
+        robot.write("67670");
+    }
 }
