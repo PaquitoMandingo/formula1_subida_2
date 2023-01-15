26dd8641ed8a99e9b1b395d5e7d1d411028c02f9
FranciscoFigueredo <figueredorubio@hotmail.com>
2023-01-15 22:29:04 +0100

    Actualizacion 15-01-2023 Faltan mas cosas

--- src/main/test/Controladors/HelloControllerTestUnitarioEliminar.java
+++ src/main/test/Controladors/HelloControllerTestUnitarioEliminar.java
@@ -1,82 +1,55 @@
 package Controladors;
 
-import com.example.formula1.HelloApplication;
-import javafx.collections.FXCollections;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxAssert;
 import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
+import org.testfx.matcher.base.WindowMatchers;
+import org.testfx.matcher.control.LabeledMatchers;
 
 import java.io.IOException;
 import java.sql.*;
 
-@ExtendWith(ApplicationExtension.class)
-class HelloControllerTestUnitario {
-    Pane mainroot;
-    Stage mainstage;
+public class HelloControllerTestUnitarioEliminar {
+    @Test()
+    void registroUnitarioEliminar() throws SQLException {
 
-
-
-    @Test
-    void registroUnitario() throws SQLException {
-        final String servidor = "jdbc:mariadb://localhost:5555/formula1?useSSL=false";
+        final String servidor = "jdbc:mariadb://localhost:5555/noinch?useSSL=false";
         final String usuario = "root";
         final String passwd = "adminer";
         int registrosAfectadosConsulta = 0;
-        Connection conexionBBDD;
-        conexionBBDD = DriverManager.getConnection(servidor, usuario, passwd);
-        String SQLINSERT = "INSERT INTO escuderia ("
-                + " CodigoEscuderia ,"
-                + " Nombre ,"
-                + " AnioCreacion ,"
-                + " Puesto, "
-                + " FechaSalida ,"
-                + " PrecioPromedio ,"
-                + " CirculatingSupply ,"
-                + " TotalSupply ,"
-                + " Contrato ,"
-                + " PaginaWeb ,"
-                + " CantidadComprada )"
-                + " VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
-
-        PreparedStatement st = null;
+        Connection con;
+        con = DriverManager.getConnection(servidor, usuario, passwd);
         try {
-            st = conexionBBDD.prepareStatement(SQLINSERT);
 
-            st.setString(1, "71710");
-            st.setString(2, "TestUnitario");
-            st.setString(3, "Test unitario e");
-            st.setInt(4, Integer.parseInt("22"));
-            st.setDate(5, Date.valueOf("2020-01-01"));
-            st.setFloat(6, Float.parseFloat("2.0"));
-            st.setFloat(7, Float.parseFloat("2.0"));
-            st.setFloat(8, Float.parseFloat("2.0"));
-            st.setString(9, "TestUnitario");
-            st.setString(10, "Test unitario e");
-            st.setFloat(11, Float.parseFloat("2.0"));
+            con = DriverManager.getConnection(servidor, usuario, passwd);
+            String SQL = "DELETE FROM criptomoneda "
+                    + " WHERE Token = 'UNI' ";
 
+            PreparedStatement st = con.prepareStatement(SQL);
 
             st.executeUpdate();
+
             st.close();
-            conexionBBDD.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error:" + e.toString());
+
         }
     }
+
     @Test
     void registroComprobar() {
         Criptomoneda c = new Criptomoneda();
         this.datosResultadoConsulta = FXCollections.observableArrayList();
-        final String servidor = "jdbc:mariadb://localhost:5555/formula1?useSSL=false";
+        final String servidor = "jdbc:mariadb://localhost:5555/noinch?useSSL=false";
         final String usuario = "root";
         final String passwd = "adminer";
         int registrosAfectadosConsulta = 0;
@@ -87,8 +60,8 @@ class HelloControllerTestUnitario {
             con = DriverManager.getConnection(servidor, usuario, passwd);
 
             String SQL = "SELECT * "
-                    + "FROM escuderias "
-                    + "WHERE codigoEscuderia ='71710'";
+                    + "FROM criptomoneda "
+                    + "WHERE Token ='UNI'";
 
 
             ResultSet resultadoConsulta = con.createStatement().executeQuery(SQL);
@@ -115,12 +88,13 @@ class HelloControllerTestUnitario {
                 System.out.println(c.toString());
 
             }
-            ResultSet resultadoConsulta2 = con.createStatement().executeQuery(SQL);
-            if(resultadoConsulta2.next()){
-                System.out.println("Registros afectados "+registrosAfectadosConsulta);
+
+            if(registrosAfectadosConsulta != 0){
+                assert false:"El campo sigue existiendo";
 
             }else {
-                assert false:"No tiene datos";
+
+                System.out.println("Se ha eliminado correctamente");
             }
 
         }catch (SQLException e){
